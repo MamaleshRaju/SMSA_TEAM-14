f26bd7ebe452db5c2f891c3b68caebd7f0e14978
Mamalesh Ramana Raju <ganapathi31071966@gmail.com>
2026-03-22 23:44:03 +0530

    Create requirements.R

---
+++ requirements.R
@@ -0,0 +1,23 @@
+# ===============================
+# REQUIRED PACKAGES
+# ===============================
+
+# Install packages (run once)
+install.packages("dplyr")
+install.packages("readr")
+install.packages("ggplot2")
+install.packages("caret")
+install.packages("randomForest")
+install.packages("e1071")
+install.packages("nnet")
+install.packages("pROC")
+
+# Load libraries
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(caret)
+library(randomForest)
+library(e1071)
+library(nnet)
+library(pROC)
